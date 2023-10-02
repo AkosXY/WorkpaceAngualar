0b52c1f3c4f70e0ef046babca5977033fc560e77
Dittrich Akos <dittrich.kos@gmail.com>
2023-10-03 00:41:53 +0200

    add basic form validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,30 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { AuthenticationService } from 'src/app/serivces/authentication.service';
 import { Router } from '@angular/router';
+/* 
+export function passwordMatchValidator(): ValidatorFn {
+  return (control): {[key: string]: any} | null => {
+    const newPass = control.get('newPasswordForm')?.value;
+    const newPassConfirm = control.get('confirmPasswordForm')?.value;
+    return newPass === newPassConfirm ? null : { passwordDontMatch: true };
+  };
+}
+ */
+
+export function passwordMatchValidator(confirmControl: AbstractControl): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const newPassword = control.value;
+    const confirmPassword = confirmControl.value;
+
+    if (newPassword !== confirmPassword) {
+      return { passwordDontMatch: true };
+    }
+
+    return null;
+  };
+}
+
 
 @Component({
   selector: 'app-login',
@@ -14,29 +37,38 @@ export class LoginComponent {
 
   constructor(private authService :AuthenticationService, private router :Router) { }
 
-  emailForm = new FormControl('');
-  passwordForm = new FormControl('');
-
   loginForm = new FormGroup({
-    emailForm: this.emailForm,
-    passwordForm: this.passwordForm
+    emailForm: new FormControl('', [Validators.email, Validators.required]),
+    passwordForm:  new FormControl('', [Validators.required, Validators.minLength(5)])
   })
 
-/*   loginForm = new FormGroup({
-    emailForm: new FormControl(''),
-    passwordForm: new FormControl('')
-  })
- */
 
-  newEmailForm = new FormControl('');
-  newPasswordForm = new FormControl('');
-  confirmPasswordForm = new FormControl('');
+  newPasswordForm = new FormControl('', [Validators.required, Validators.minLength(5)]);
+  confirmPasswordForm = new FormControl('', [Validators.required, Validators.minLength(5), passwordMatchValidator(this.newPasswordForm)]);
 
   signupForm = new FormGroup({
-    newEmailForm: this.newEmailForm,
+    newEmailForm: new FormControl('', [Validators.email, Validators.required]),
     newPasswordForm: this.newPasswordForm,
     confirmPasswordForm: this.confirmPasswordForm
-  })
+  });
+
+  get emailForm() {
+    return this.loginForm.get('emailForm');
+  }
+
+  get passwordForm() {
+    return this.loginForm.get('passwordForm');
+  }
+
+
+  get newEmailForm() {
+    return this.signupForm.get('newEmailForm');
+  }
+
+  passwordsMatch(): boolean {
+    return this.newPasswordForm?.value === this.confirmPasswordForm?.value;
+  }
+  
 
   login(){
     this.router.navigateByUrl('/home');
